a8fa049d9bacd4390a6144046c5aa74e51db2cb3
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-05 18:19:29 +0700

    Update Sudo.py

--- Sudo.py
+++ Sudo.py
@@ -19,39 +19,39 @@
 
 from telethon.tl.types import User
 
-from Kazu._misc import sudoers
+from dante._misc import sudoers
 
-from . import get_string, inline_mention, udB, kazu_bot, kazu_cmd
+from . import get_string, inline_mention, udB, dante_bot, dante_cmd
 
 
-@kazu_cmd(pattern="addsudo( (.*)|$)", fullsudo=False)
-async def _(kazu):
-    inputs = kazu.pattern_match.group(1).strip()
-    if kazu.reply_to_msg_id:
-        replied_to = await kazu.get_reply_message()
+@dante_cmd(pattern="addsudo( (.*)|$)", fullsudo=False)
+async def _(dante):
+    inputs = dante.pattern_match.group(1).strip()
+    if dante.reply_to_msg_id:
+        replied_to = await dante.get_reply_message()
         id = replied_to.sender_id
         name = await replied_to.get_sender()
     elif inputs:
         try:
-            id = await kazu.client.parse_id(inputs)
+            id = await dante.client.parse_id(inputs)
         except ValueError:
             try:
                 id = int(inputs)
             except ValueError:
                 id = inputs
         try:
-            name = await kazu.client.get_entity(int(id))
+            name = await dante.client.get_entity(int(id))
         except BaseException:
             name = None
-    elif kazu.is_private:
-        id = kazu.chat_id
-        name = await kazu.get_chat()
+    elif dante.is_private:
+        id = dante.chat_id
+        name = await dante.get_chat()
     else:
-        return await kazu.eor(get_string("sudo_1"), time=5)
+        return await dante.eor(get_string("sudo_1"), time=5)
     if name and isinstance(name, User) and (name.bot or name.verified):
-        return await kazu.eor(get_string("sudo_4"))
+        return await dante.eor(get_string("sudo_4"))
     name = inline_mention(name) if name else f"`{id}`"
-    if id == kazu_bot.uid:
+    if id == dante_bot.uid:
         mmm = get_string("sudo_2")
     elif id in sudoers():
         mmm = f"{name} `sudah menjadi Pengguna SUDO ...`"
@@ -61,33 +61,33 @@ async def _(kazu):
         key.append(id)
         udB.set_key("SUDOS", key)
         mmm = f"**Ditambahkan** {name} **sebagai Pengguna SUDO**"
-    await kazu.eor(mmm, time=5)
+    await dante.eor(mmm, time=5)
 
 
-@kazu_cmd(pattern="delsudo( (.*)|$)", fullsudo=False)
-async def _(kazu):
-    inputs = kazu.pattern_match.group(1).strip()
-    if kazu.reply_to_msg_id:
-        replied_to = await kazu.get_reply_message()
+@dante_cmd(pattern="delsudo( (.*)|$)", fullsudo=False)
+async def _(dante):
+    inputs = dante.pattern_match.group(1).strip()
+    if dante.reply_to_msg_id:
+        replied_to = await dante.get_reply_message()
         id = replied_to.sender_id
         name = await replied_to.get_sender()
     elif inputs:
         try:
-            id = await kazu.client.parse_id(inputs)
+            id = await dante.client.parse_id(inputs)
         except ValueError:
             try:
                 id = int(inputs)
             except ValueError:
                 id = inputs
         try:
-            name = await kazu.client.get_entity(int(id))
+            name = await dante.client.get_entity(int(id))
         except BaseException:
             name = None
-    elif kazu.is_private:
-        id = kazu.chat_id
-        name = await kazu.get_chat()
+    elif dante.is_private:
+        id = dante.chat_id
+        name = await dante.get_chat()
     else:
-        return await kazu.eor(get_string("sudo_1"), time=5)
+        return await dante.eor(get_string("sudo_1"), time=5)
     name = inline_mention(name) if name else f"`{id}`"
     if id not in sudoers():
         mmm = f"{name} `bukan Pengguna SUDO ...`"
@@ -96,20 +96,20 @@ async def _(kazu):
         key.remove(id)
         udB.set_key("SUDOS", key)
         mmm = f"**DIHAPUS** {name} **dari Pengguna SUDO(s)**"
-    await kazu.eor(mmm, time=5)
+    await dante.eor(mmm, time=5)
 
 
-@kazu_cmd(
+@dante_cmd(
     pattern="listsudo$",
 )
-async def _(kazu):
+async def _(dante):
     sudos = sudoers()
     if not sudos:
-        return await kazu.eor(get_string("sudo_3"), time=5)
+        return await dante.eor(get_string("sudo_3"), time=5)
     msg = ""
     for i in sudos:
         try:
-            name = await kazu.client.get_entity(int(i))
+            name = await dante.client.get_entity(int(i))
         except BaseException:
             name = None
         if name:
@@ -118,7 +118,7 @@ async def _(kazu):
             msg += f"• `{i}` -> Pengguna tidak valid\n"
     m = udB.get_key("SUDO") or True
     if not m:
-        m = "[False](https://graph.org/kazu-11-29)"
+        m = "[False](https://graph.org/dante-11-29)"
     return await kazu.eor(
         f"**SUDO MODE : {m}\n\nList of SUDO Users :**\n{msg}", link_preview=False
     )
